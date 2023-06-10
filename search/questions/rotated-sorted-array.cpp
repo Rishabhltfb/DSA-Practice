09746b40536e1eac003ecf5ca8283c577adb000e
Rishabhltfb <rishabhsharma.rs0403@gmail.com>
2023-06-10 23:37:21 +0530

    solution: search in rotated sorted array

---
+++ search/questions/rotated-sorted-array.cpp
@@ -0,0 +1,34 @@
+// Link: https://bit.ly/3OmIp5d
+
+int searchInRotatedSortedArray(vector<int>& arr, int n, int k)
+{
+    int low = 0, high = n-1;
+    while(low<=high){
+        int mid = low + (high-low)/2;
+        int midElement = arr[mid];
+        int lowElement = arr[low];
+        int highElement = arr[high];
+        bool leftSorted = lowElement<= midElement;
+        bool goRight = false;
+        if(midElement==k) return mid;
+        if(leftSorted ){
+            if(lowElement<= k && k<=midElement){
+                goRight = false;
+            }else{
+                goRight= true;
+            }
+        }else{
+            if(midElement<=k && k<=highElement){
+                goRight = true;
+            }else{
+                goRight= false;
+            }
+        }
+        if(goRight){
+            low = mid +1;
+        }else{
+            high = mid -1;
+        }
+    }
+    return -1;
+}
