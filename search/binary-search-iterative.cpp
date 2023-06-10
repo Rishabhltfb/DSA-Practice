0799bdd09b6468594b9a1a80de259fe66fdd6c09
Rishabhltfb <rishabhsharma.rs0403@gmail.com>
2023-06-10 19:15:00 +0530

    edge-case: prevent int overflow

--- search/binary-search-iterative.cpp
+++ search/binary-search-iterative.cpp
@@ -2,7 +2,8 @@ int binarySearch(vector<int>& nums, int target) {
     // Write Your Code Here
     int low = 0, high = nums.size()-1;
     while (low<=high){
-        int mid = (low +high)/2;
+        // int mid = (low +high)/2; // have overflow issue in edge cases
+        int mid = low + (high-low)/2; // to prevent overflow condition because int has limit to store a max value
         if(nums[mid] == target){
             return mid;
         }else if(nums[mid] <target){
--- search/binary-search-recursive.cpp
+++ search/binary-search-recursive.cpp
@@ -1,6 +1,7 @@
 int recursiveSearch(vector<int>& nums, int low, int high, int target){
   if(low>high) return -1; // base case
-    int mid = (low+high)/2;
+    // int mid = (low+high)/2;  // have overflow issue in edge cases
+     int mid = low + (high-low)/2; // to prevent overflow condition because int has limit to store a max value  
     if(nums[mid] == target) return mid;
     else if(nums[mid] > target) return recursiveSearch(nums,  low,  mid-1,  target);
     else return recursiveSearch(nums,  mid+1,  high,  target);
