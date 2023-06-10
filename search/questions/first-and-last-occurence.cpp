359616bc7c3a39a179c4f603a8351cff1c540cf8
Rishabhltfb <rishabhsharma.rs0403@gmail.com>
2023-06-10 23:03:00 +0530

    solution: first & last occurence of x

---
+++ search/questions/first-and-last-occurence.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k)
+{
+    // Write your code here
+    auto lowerBound = lower_bound(arr.begin(), arr.end(), k);
+    int lowerBoundIndex = lowerBound - arr.begin();
+    if(lowerBoundIndex>n-1 || arr[lowerBoundIndex]!= k){
+        return {-1,-1};
+    }
+    auto upperBound = upper_bound(arr.begin(), arr.end(), k);
+    int upperBoundIndex = upperBound - arr.begin();
+    return {lowerBoundIndex, upperBoundIndex-1};
+}
