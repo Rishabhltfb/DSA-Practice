1c8ae27b6c24ae150d8a1f484900d5efaed502ed
Rishabhltfb <rishabhsharma.rs0403@gmail.com>
2023-06-12 00:33:37 +0530

    solution: intersection of sorted arrays

---
+++ array/questions/intersection-of-sorted-arrays.cpp
@@ -0,0 +1,26 @@
+// LINK: http://bit.ly/3KSSx3Z
+// DIFICULTY: EASY-MEDIUM
+
+#include <bits/stdc++.h> 
+
+	bool search()
+
+vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
+{
+	vector<int> intersectionArr;
+	int i=0,j=0;
+	while(i<n && j<m){
+		if(arr1[i]==arr2[j]) {
+				intersectionArr.push_back(arr1[i]);
+			i++;
+			j++;
+		}else{
+			if(arr1[i]< arr2[j]){
+				i++;
+			}else{
+				j++;
+			}
+		}
+	}
+	return intersectionArr;
+}
