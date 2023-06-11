ca7a3a16318caaf7a9f91c79d2e733835a07c727
Rishabhltfb <rishabhsharma.rs0403@gmail.com>
2023-06-12 00:33:20 +0530

    solution: union of sorted arrays

---
+++ array/questions/union-of-sorted-arrays.cpp
@@ -0,0 +1,36 @@
+// LINK: https://bit.ly/3IpS7Rd
+// DIFICULTY: EASY-MEDIUM
+
+vector < int > unionOfSortedArrays(vector < int > a, vector < int > b) {
+    // Write your code here
+    int n1 = a.size();
+    int n2 = b.size();
+    vector<int> unionArr;
+    int i =0, j = 0;
+    while(i<n1 && j<n2){
+        if(a[i]<=b[j]){
+            if(unionArr.size()==0 || unionArr.back()!= a[i]){
+                unionArr.push_back(a[i]);
+            }
+            i++;
+        }else{
+            if(unionArr.size()==0 || unionArr.back()!= b[j]){
+                unionArr.push_back(b[j]);
+            }
+            j++;
+        }
+    }
+    while(i<n1){
+        if(unionArr.size()==0 || unionArr.back()!= a[i]){
+                unionArr.push_back(a[i]);
+            }
+            i++;
+    }
+    while(j<n2){
+        if(unionArr.size()==0 || unionArr.back()!= b[j]){
+                unionArr.push_back(b[j]);
+            }
+            j++;
+    }
+    return unionArr;
+}
